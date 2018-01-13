185464cb0abc0a5f727bd09d8cdbb06be477a78b
Olya Yakymovych <32932692+explicity@users.noreply.github.com>
2018-01-13 10:51:45 +0200

    corrected portfolio heading

--- my-app/src/components/portfolio-grid page/PortfolioHeading.js
+++ my-app/src/components/portfolio-grid page/PortfolioHeading.js
@@ -4,19 +4,19 @@ import React, { Component } from 'react';
 class PortfolioHeading extends Component {
   render() {
     return (
-      <section class="title-blog">
-        <div class="title-blog-main">
-          <div class="container">
+      <section className="title-blog">
+        <div className="title-blog-main">
+          <div className="container">
 
-            <div class="title-blog-secondary d-flex justify-content-between align-items-center">
+            <div className="title-blog-secondary d-flex justify-content-between align-items-center">
               <h2>Portfolio grid gallery</h2>
               
-              <div aria-label="breadcrumb" role="navigation" class="d-flex align-items-center">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="">Home</a></li>
-                  <li class="breadcrumb-item"><a href="">Shortcodes</a></li>
-                  <li class="breadcrumb-item"><a href="">Typography</a></li>                
-                  <li class="breadcrumb-item"><a href="">Blog</a></li>
+              <div aria-label="breadcrumb" role="navigation" className="d-flex align-items-center">
+                <ol className="breadcrumb">
+                  <li className="breadcrumb-item"><a href="">Home</a></li>
+                  <li className="breadcrumb-item"><a href="">Shortcodes</a></li>
+                  <li className="breadcrumb-item"><a href="">Typography</a></li>                
+                  <li className="breadcrumb-item"><a href="">Blog</a></li>
                 </ol>
               </div>
 
